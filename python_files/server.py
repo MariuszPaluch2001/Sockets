0dfeff18a84267c69a02d90481cf489a1260ec46
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-10-06 19:53:11 +0200

    Add client.

--- python_files/server.py
+++ python_files/server.py
@@ -17,21 +17,23 @@ def handle_client(conn, addr):
     print(f"New connection: {addr} connected.")
     connected = True
     while connected:
-        msg_length = int(conn.recv(HEADER).decode(FORMAT))
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
-        print(f"[{addr}] {msg}")
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}")
     
     conn.close()
-    
+
 def start():
     server.listen()
+    print(f"SERVER is listening on {SERVER}")
     while True:
-        addr, conn = server.accept()
+        conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args = (conn,addr))
         thread.start()
         print(f"Active connections: {threading.active_count() - 1}")
 
-print("Server is running...")
 start()
